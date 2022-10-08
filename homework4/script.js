7363e18989a3421c560cae377d3ad7efaeddee4e
- <->
2022-10-08 08:05:29 +0300

    adding curly braces

--- homework4/script.js
+++ homework4/script.js
@@ -1,28 +1,28 @@
-const sum = (a, b) => a + b;
-console.log(sum(1, 4));
+// const sum = (a, b) => a + b;
+// console.log(sum(1, 4));
 
-function sum (a, b) {
-    return a + b;
-};
+// function sum (a, b) {
+//     return a + b;
+// };
 
-console.log(sum(4, 2));
+// console.log(sum(4, 2));
 
 let userName = prompt('Как Вас зовут?');
 let userAge = prompt('Сколько Вам лет?');
 function userGreeting () {
-    if(userAge <= 30)
-    alert('Привет' + ' ' + userName);
-    else
-    alert('Здравствуйте' + ' ' + userName)
+    if(userAge <= 30) {
+    alert('Привет' + ' ' + userName)
+     } else {
+    alert('Здравствуйте' + ' ' + userName); }
 };
 userGreeting(userName)
 
-function expNumber(a, b = 2) {
-    const result = a ** b;
-    return result;
-};
+// function expNumber(number, exp = 2) {
+//     const result = number ** exp;
+//     return result;
+// };
 
-console.log(expNumber(2, 8));
+// console.log(expNumber(2, 8));
 
-const expNumber = (a, b = 2) => a ** b;
+const expNumber = (number, exp = 2) => number ** exp;
 console.log(expNumber(2));
