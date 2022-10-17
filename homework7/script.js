833a1efb780fcac2570804f06fd331c69149e829
- <->
2022-10-17 16:33:02 +0300

    correction timetable code

--- homework7/script.js
+++ homework7/script.js
@@ -1,47 +1,45 @@
-// const timeTable = {}
+const timeTable = {}
 
-// const sheet = () => {
-//     let time;
-//     let active;
+const sheet = () => {
+    let time;
+    let active;
     
-//     do {
-//         time = prompt('Время суток')
-//         if(!time) {
-//             alert('Укажите время');
-//             continue
-//         };
-//         active = prompt('Что Вы хотите делать?')
-//         if(!active) {
-//             alert('Укажите занятие');
-//             continue
-//         };
+    do {
+        time = prompt('Время суток')
+        if(!time) {
+            alert('Укажите время');
+            continue
+        };
+        active = prompt('Что Вы хотите делать?')
+        if(!active) {
+            alert('Укажите занятие' ,);
+            continue
+        };
         
-//         timeTable[time] = active; 
+        timeTable[time] = active; 
            
-//     } while ((time && active) !== null) {
-//         timeTable[time] = active
-//        };
+    } while ((time && active) !== null)
          
-//        return(timeTable);
+       return(timeTable);
      
-// }
-// sheet()
+}
+sheet()
 
-// console.log(timeTable) 
+console.log(timeTable) 
 
 
-const salaries = {
-    John: "4300.00",
-    Ann: "5700.40",
-    Pete: "4900.95",
-  };
-  const getSalariesTotal = (salaries) => {
-    let total = 0;
-    for(let emploee in salaries) {
-        total += Number(salaries[emploee]);
-    }
-    return total.toFixed(2);
-  };
+// const salaries = {
+//     John: "4300.00",
+//     Ann: "5700.40",
+//     Pete: "4900.95",
+//   };
+//   const getSalariesTotal = (salaries) => {
+//     let total = 0;
+//     for(let emploee in salaries) {
+//         total += Number(salaries[emploee]);
+//     }
+//     return total.toFixed(2);
+//   };
 
-  const totalResult = getSalariesTotal(salaries)
-  console.log(totalResult)
+//   const totalResult = getSalariesTotal(salaries)
+//   console.log(totalResult)
