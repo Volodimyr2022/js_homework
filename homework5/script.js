5065c01fa37743e287629915ca9297901a546315
- <->
2022-10-20 21:34:02 +0300

    exeption 1 from list in task 2

--- homework5/script.js
+++ homework5/script.js
@@ -1,110 +1,57 @@
-// const LOGIN = 'ADMIN';
-// const PASSWORD = '1q2w3e';
-
-// const authorize = () => {
-//     let userPassword;
-//     let userLogin;
-//     do {
-//         userLogin = prompt('Логин') 
-//             if (!userLogin) {
-//                 alert('Введите логин');
-//             continue
-//             };
-//             userPassword = prompt('Пароль') 
-//             if (!userPassword) {
-//                 alert('Введите пароль');
-//             continue
-//             };
-//             if (userPassword !== PASSWORD || userLogin !== LOGIN) {
-//                 alert('Данные не верны');
-//             continue
-//             };
-//             break;
-//     }while(true)
-//     alert('Welcome!')
-// }
-// authorize()
 
 
-// const LOGIN = 'ADMIN';
-// const PASSWORD = '1q2w3e';
+const LOGIN = 'ADMIN';
+const PASSWORD = '1q2w3e';
 
-// const authorize = () => {
-//     let userPassword;
-//     let userLogin;
-//     let isAuthSuccess = false;
-
-//     do {
-//         userLogin = prompt('Логин:');
-//             if (!userLogin) {
-//                     alert('Введите логин');
-//                     continue;
-//                 }
+const authorize = () => {
+    let userPassword;
+    let userLogin;
+    let isAuthSuccess = false;
+    let attemptsLeft = 2;
+    let i = 0;
+    do {
+        userLogin = prompt('Логин:');
+            if (!userLogin) {
+                    alert('Введите логин');
+                    continue;
+                }
             
-//         userPassword = prompt('Пароль:'); 
-//             if (!userLogin) {
-//                     alert('Введите пароль');
-//                     continue;
-//                 }
-//             if (userLogin === LOGIN && userPassword === PASSWORD) {
-//                 isAuthSuccess = true;
-//                 } else {
-//                     alert('Данные не верны')
-//                 };      
-//         } while(!isAuthSuccess); 
-//     alert('Welcome!');
-// };
-// authorize()
+        userPassword = prompt('Пароль:'); 
+            if (!userLogin) {
+                    alert('Введите пароль');
+                    continue;
+                }
+            if (userLogin === LOGIN && userPassword === PASSWORD) {
+                isAuthSuccess = true;
+                alert('Welcome!');
+                } 
+                else if (attemptsLeft > i) {
+                alert(`Данные не верны у вас осталось ${attemptsLeft - i} попыток`)
+                    i++
+                } else { 
+                    alert('Попытки закончились');
+                    break;
+                }      
+        } while(!isAuthSuccess);
+};
+authorize()
 
-// const LOGIN = 'ADMIN';
-// const PASSWORD = '1q2w3e';
+// let a;
+// let b;
+// const simpleNumber = (a,b) => {
+//   if(a == 1){
+//     a++
+//   }
+//   else a;
+// notSimple:
+// for (let i = a; i <= b; i++) {
 
-// const authorize = () => {
-//     let userPassword;
-//     let userLogin;
-//     let isAuthSuccess = false;
-//     let attemptsLeft = 2;
-//     let i = 0;
-//     do {
-//         userLogin = prompt('Логин:');
-//             if (!userLogin) {
-//                     alert('Введите логин');
-//                     continue;
-//                 }
-            
-//         userPassword = prompt('Пароль:'); 
-//             if (!userLogin) {
-//                     alert('Введите пароль');
-//                     continue;
-//                 }
-//             if (userLogin === LOGIN && userPassword === PASSWORD) {
-//                 isAuthSuccess = true;
-//                 alert('Welcome!');
-//                 } 
-//                 else if (attemptsLeft > i) {
-//                 alert(`Данные не верны у вас осталось ${attemptsLeft - i} попыток`)
-//                     i++
-//                 } else { 
-//                     alert('Попытки закончились');
-//                     break;
-//                 }      
-//         } while(!isAuthSuccess);
-// };
-// authorize()
-
-let a;
-let b;
-const simpleNumber = (a,b) => {
-  
-notSimple:
-for (let i = a; i <= b; i++) {
-
-  for (let j = 2; j < i; j++) { 
-    if (i % j == 0) continue notSimple;
+//   for (let j = 2; j < i; j++) { 
+//     if (i % j == 0) continue notSimple;
     
-  }
-  console.log( i ); 
-}
-}
+//   }
+//   console.log( i ); 
+// }
+// }
 
-simpleNumber(30, 40)
+// simpleNumber(1, 40)
