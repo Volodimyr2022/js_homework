533bf8123fad2a3591496bfeba98360d9a4766a7
- <->
2022-10-25 21:23:50 +0300

    corrected solution task 1, solution task 3

--- homework8/script.js
+++ homework8/script.js
@@ -2,14 +2,41 @@ const exam = [true, false, false, true, false];
 
 
 function getTrueAmount(){
-    let sum = 0;
+    let sum = [];
     for(let trueValue of exam) {
         if(trueValue === true) {
-            sum++;
+            sum.push(trueValue);
         }  
     }
-    return sum;
+    return sum.length;
 }
 
 const result = getTrueAmount(exam);
-console.log(result)
+console.log(result);
+
+ 
+
+// const num = [2, 6, 8, 10, 3];
+// const getEvenOdd = (arr) => {
+        
+//     let even = [];
+//     let odd = [];
+//     let res 
+//     for(let number of arr) {
+//         if(number%2 === 0) { 
+//            even.push(number) 
+//         } else {
+//             odd.push(number)
+//         }        
+//         if(even.length === 1)  
+//         {
+//              res = even[0]
+//         }else{
+//              res = odd[0]
+//         }
+//     }
+//     return res
+// }
+// const resEvenOdd = getEvenOdd(num);
+// console.log(resEvenOdd)
+
