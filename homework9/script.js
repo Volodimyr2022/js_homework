e959d7bad058f841a28d91af67372f08980e4d2e
- <->
2022-11-02 19:43:37 +0200

    solution task3

--- homework9/script.js
+++ homework9/script.js
@@ -40,10 +40,13 @@
 //     },
 //   ]
 
-// const adultAdmin = users
+// const adultAdmin = (userArr) => {
+// return userArr    
 // .filter((userData) => (userData.age > 18 && userData.role === 'admin'))
 // .map((userData) => userData.name)
-// console.log(adultAdmin)
+// }
+// const res = adultAdmin(users)
+// console.log(res)
 
 // const numbers = [1, 10, 17, 24, 45];
 // const middleNumber = numbers.reduce((sum, current) => sum + current) / numbers.length;
@@ -92,63 +95,83 @@
 
 
 
-// const data = [
-//   {
-//     name: "John",
-//     age: 24,
-//     position: "senior",
-//     isActive: false,
-//   },
-//   {
-//     name: "Peter",
-//     age: 33,
-//     position: "middle",
-//     isActive: false,
-//   },
-//   {
-//     name: "Sam",
-//     age: 29,
-//     position: "junior",
-//     isActive: true,
-//   },
-//   {
-//     name: "Mary",
-//     age: 24,
-//     position: "middle",
-//     isActive: false,
-//   },
-//   {
-//     name: "Steve",
-//     age: 23,
-//     position: "middle",
-//     isActive: true,
-//   },
-//   {
-//     name: "Kate",
-//     age: 31,
-//     position: "middle",
-//     isActive: false,
-//   },
-//   {
-//     name: "Sally",
-//     age: 19,
-//     position: "junior",
-//     isActive: false,
-//   },
-//   {
-//     name: "Jack",
-//     age: 19,
-//     position: "middle",
-//     isActive: true,
-//   },
-// ];
+const data = [
+  {
+    name: "John",
+    age: 24,
+    position: "senior",
+    isActive: false,
+  },
+  {
+    name: "Peter",
+    age: 33,
+    position: "middle",
+    isActive: false,
+  },
+  {
+    name: "Sam",
+    age: 29,
+    position: "junior",
+    isActive: true,
+  },
+  {
+    name: "Mary",
+    age: 24,
+    position: "middle",
+    isActive: false,
+  },
+  {
+    name: "Steve",
+    age: 23,
+    position: "middle",
+    isActive: true,
+  },
+  {
+    name: "Kate",
+    age: 31,
+    position: "middle",
+    isActive: false,
+  },
+  {
+    name: "Sally",
+    age: 19,
+    position: "junior",
+    isActive: false,
+  },
+  {
+    name: "Jack",
+    age: 19,
+    position: "middle",
+    isActive: true,
+  },
+];
 
 
-//   const filterData = (arr, obj) => {              
-//     const sort = data.filter((point) => point.obj) 
-    
-  
-//   };
-//   const res = filterData(data, {position: "middle"})  
-  
+  // const filterData = (dataArr, templateObjectForFilter) => {              
+  //   let sort = dataArr.filter(point => {
+  //       console.log('keys:', Object.keys(templateObjectForFilter))
+  //       return Object.keys(templateObjectForFilter).every(key => {
+  //         console.log(point[key], 'и', templateObjectForFilter[key], '=', point[key] === templateObjectForFilter[key])  
+  //           return point[key] === templateObjectForFilter[key]
+  //       })
+  //   }); 
+  // return sort 
+  // };
+  // const resFilter = filterData(data, { position: "middle", age: 19, isActive: true })  
+  // console.log(resFilter)
+                                 
+  const filterData = (dataArr, templateObjectForFilter) => {              
+    const sort = dataArr.filter(point => {
+      console.log('keys:', Object.keys(templateObjectForFilter))
+        for(let key in point) {
+          console.log(point[key], 'и', templateObjectForFilter[key], '=', point[key] === templateObjectForFilter[key])
+         if(point[key] === templateObjectForFilter[key]) {
+          return key
+         }
+        }
+      });
+    return sort 
+  };
+  const resFilter = filterData(data, { position: "middle", age: 19, isActive: true })  
+  console.log(resFilter)
                                  
