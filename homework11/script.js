a487b7aa1e24105ade7e6c4642b3c7adc7f90330
- <->
2022-11-17 21:30:07 +0200

    homework11

--- homework11/script.js
+++ homework11/script.js
@@ -1,46 +1,84 @@
-// const products = [
-//     {
-//       name: "Iphone 12",
-//       brand: "Apple",
-//       price: 3200000,
-//       properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
-//     },
-//     {
-//       name: "Galaxy S20 Ultra",
-//       brand: "Samsung",
-//       price: 2900000,
-//       properties: ["120 hz screen", "Water resistance"],
-//     },
-//     {
-//       name: "MI 9",
-//       brand: "Xiaomi",
-//       price: 1300000,
-//       properties: ["Best price", "Pay less - get more!"],
-//     },
-//   ];
+const products = [
+    {
+      name: "Iphone 12",
+      brand: "Apple",
+      price: 3200000,
+      properties: ["Best camera", "Fast memory", "Apple A12 Processor"],
+    },
+    {
+      name: "Galaxy S20 Ultra",
+      brand: "Samsung",
+      price: 2900000,
+      properties: ["120 hz screen", "Water resistance"],
+    },
+    {
+      name: "MI 9",
+      brand: "Xiaomi",
+      price: 1300000,
+      properties: ["Best price", "Pay less - get more!"],
+    },
+  ];
 
-const createCardProduct = (name, brand, price, properties) => {
-    const cardProductContainer = document.createElement('div');
-    const titleElement = document.createElement('h2');
-    const subtitleElement = document.createElement('h3');
-    const priceElement = document.createElement('p');
-    const textContainer = document.createElement('ul');
+// const createCardProduct = (productsList) => {
 
-    titleElement.innerText = name;
-    subtitleElement.innerText = brand;
-    priceElement.innerText = price;
+//     const container = document.createElement('div');
 
-    properties.forEach((paragraph) => {
-        const paragraphElement =  document.createElement('li');
-        paragraphElement.innerText = paragraph;
+//     productsList.forEach((product) => {  
 
-        textContainer.append(paragraphElement)       
-    });
+//     const cardProductContainer = document.createElement('div');
+//     const titleElement = document.createElement('h2');
+//     const subtitleElement = document.createElement('h3');
+//     const priceElement = document.createElement('p');
+//     const textContainer = document.createElement('ul');
 
-    cardProductContainer.append(titleElement, subtitleElement, priceElement, textContainer); 
-    return cardProductContainer;
+//     titleElement.innerText = product.name;
+//     subtitleElement.innerText = product.brand;
+//     priceElement.innerText = product.price;
+
+//     product.properties.forEach((prop) => {
+//         const paragraphElement =  document.createElement('li');
+//         paragraphElement.innerText = prop;
+
+//         textContainer.append(paragraphElement)       
+//     });
+
+//     cardProductContainer.append(titleElement, subtitleElement, priceElement, textContainer); 
+    
+//     container.append(cardProductContainer);
+// });
+//   document.body.append(container)
+// };
+
+// const res = createCardProduct(products);
+
+
+
+const createCardProduct = (arrayProducts) => {
+    
+    const container = document.createElement('div')
+
+    container.innerHTML = `
+    ${arrayProducts
+      .map((oneProduct) => {
+       return `
+        <h2>${oneProduct.name}</h2>
+        <h3>${oneProduct.brand}</h3>
+        <p>${oneProduct.price}</p>
+        <ul>
+            ${oneProduct.properties
+                .map((propItem) => {
+                  return `
+                <li>
+                ${propItem}
+                </li>
+                `
+              })
+                .join('')}
+        </ul>
+    `})
+    .join('')}
+` 
+document.body.append(container)    
 };
 
-const res = createCardProduct('Iphone 12', 'Apple', 3200000, ['Best camera', 'Fast memory', 'Apple A12 Processor']);
-document.body.append(res)
-
+const res = createCardProduct(products);
