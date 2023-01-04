421d93b31d2f7d55072bc92816b52e6faf98f7c1
- <->
2023-01-04 07:05:20 +0200

    add solution with class

--- homework20/script.js
+++ homework20/script.js
@@ -37,6 +37,45 @@ CommunalPayments.prototype.getSum = function(){
     return sum
 }
 
+
+// class CommunalPayments {
+//     constructor(services, rates){
+//         this.services = [];
+//         console.log(this.services)
+//             this.rates = {
+//                 hotWater: 7,
+//                 coldWater: 1,
+//                 gas: 0.3,
+//                 electricity: 1.6,
+//               };
+//               console.log(this.rates) 
+//     }
+//     addPayment(amount, serviceName){
+//         if(!Object.keys(this.rates).includes(serviceName)){
+//             throw new Error(`Service "${serviceName}" is unavailable.`)
+//         }
+    
+//         if(this.services.some(({key}) => key ===  serviceName)){
+//             throw new Error(`Service "${serviceName}" is already included.`)
+//         }
+    
+//         this.services.push({key: serviceName, amount}) 
+//     }
+//     deletePayment(serviceName){
+//         this.services = this.services.filter(({key}) => key !== serviceName)
+//     }
+//     getSum(){
+//         let sum = 0;
+
+//         this.services.forEach(({key, amount}) => {
+//             sum += this.rates[key] * amount
+//             console.log(this.rates[key])
+//             console.log(amount)
+//         })
+//         return sum
+//     }
+// }
+
 const payment = new CommunalPayments();
 
 payment.addPayment(20, "hotWater");
